736eb1cf2335834c4087bfad608dc6cd2637c182
Heather Drattlo <hdrattlo88@yahoo.com>
2018-11-28 17:59:49 -0700

    update

--- app/config/connection.js
+++ app/config/connection.js
@@ -21,8 +21,8 @@ if (process.env.JAWSDB_URL){
   connection=mysql.createConnection({
     host:'localhost',
     user: 'root',
-    password: 'hacktheplanet',
-    database: 'todoagain_db'
+    password: '',
+    database: 'seat_yourself'
   });
 
 };
